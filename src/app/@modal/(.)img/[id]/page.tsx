3e2e6c78eefaf5f4879c1d43151d9becd3aaef36
finalManu <manuelrydholm@gmail.com>
2025-02-03 15:32:37 +0100

    dialog fullscreen, image view component

--- src/app/@modal/(.)img/[id]/page.tsx
+++ src/app/@modal/(.)img/[id]/page.tsx
@@ -1,5 +1,5 @@
-import { getImage } from "~/server/queries";
 import { Modal } from "./modal";
+import FullPageImageView from "~/app/components/full-page-image";
 
 export default async function ImageModal({
   params,
@@ -11,10 +11,9 @@ export default async function ImageModal({
   const idAsNumber = Number(imageId);
   if (Number.isNaN(idAsNumber)) throw new Error("Invalid photo id");
 
-  const image = await getImage(idAsNumber);
   return (
     <Modal>
-      <img src={image.url} className="w-96" alt="" />
+      <FullPageImageView id={idAsNumber} />
     </Modal>
   );
 }
--- src/app/img/[id]/page.tsx
+++ src/app/img/[id]/page.tsx
@@ -1,8 +1,14 @@
-export default async function ImageModal({
+import FullPageImageView from "~/app/components/full-page-image";
+
+export default async function ImagePage({
   params,
 }: {
   params: Promise<{ id: string }>;
 }) {
   const imageId = (await params).id;
-  return <div>{imageId}</div>;
+
+  const idAsNumber = Number(imageId);
+  if (Number.isNaN(idAsNumber)) throw new Error("Invalid photo id");
+
+  return <FullPageImageView id={idAsNumber} />;
 }
